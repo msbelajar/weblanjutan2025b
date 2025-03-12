666762aae1398251cf6cc9d9b30be83d79ab6bdf
muhsadno <1526050+msadno@users.noreply.github.com>
2025-03-12 09:13:39 +0800

    "KLIK"

--- src/03/ContohEvent/Button.jsx
+++ src/03/ContohEvent/Button.jsx
@@ -1,7 +1,12 @@
 function Button() {
+
+  function handleClick() {
+    console.log("klik")
+  }
+
   return (
     <div>
-        <button className="bg-sky-500 px-4 py-2 rounded">Click</button>
+        <button onClick={handleClick} className="bg-sky-500 px-4 py-2 rounded">Click</button>
     </div>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Button from "./03/ContohEvent/Button"
+import Button from "./03/ContohEvent/Button";
 
 export default function App() {
+  
   return (
     <div className="container mt-3 mx-auto">
       <Button />
     </div>
-  )
+  );
 }
